0a3e0e7487f708d4b8f006aa2c075a632f345be5
a-boyadzhiev <150182926+a-boyadzhiev@users.noreply.github.com>
2023-12-02 09:18:32 +0200

    Create a function that randomly returns eturn ‘Rock’, ‘Paper’ or ‘Scissors’.

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+// Function that chooses randomly
 
+const array = ["ROCK", "PAPER", "SCISSORS"];
+
+      function getComputerChoice (array) {
+        const randomChoice = Math.floor(Math.random() * array.length)
+        const choice = array[randomChoice]
+        return choice;
+      }
