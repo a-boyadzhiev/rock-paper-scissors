041d2f5d4685e55e30ddabe5db9a76271af7a246
a-boyadzhiev <150182926+a-boyadzhiev@users.noreply.github.com>
2023-12-12 22:11:46 +0200

    Fix typos and change variables

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ let compChoice = {Value: ""};
 let playerChoice;
 let compChoiceInt = 0;
 let playerChoiceInt = 0;
-const images = document.querySelectorAll('.img');
+const buttons = document.querySelectorAll('.btn');
 
-let computerScore = 0;
 let playerScore = 0;
+let compScore = 0;
 
 const player = document.querySelector("#player-score");
 player.textContent = `Player Score: ${playerScore}`;
@@ -16,31 +16,29 @@ computer.textContent = `Computer Score: ${compScore}`;
 const output = document.querySelector("#output");
 output.textContent = "May the Best Person Win!"
 
-
-images.forEach((img) => {img.addEventListener('click', () => {
-    playerChoice = img.id;
+buttons.forEach((button) => {button.addEventListener('click', () => {
+    playerChoice = button.id;
     if (playerChoice == "rock"){playerChoiceInt = 0;}
     else if (playerChoice == "paper"){playerChoiceInt = 1;}
     else if (playerChoice == "scissors"){playerChoiceInt = 2;}
 
-    compChoiceInt = getComputerChoice(array);
-    
+    compChoiceInt = computerPlay(compChoice);
+    playGame ()
   }) 
 });
 
-function computerPlay (compChoice) {
+function computerPlay(compChoice) {
   let choiceNum = Math.floor(Math.random() * 3)
 
-  if (choiceNum == 0){compChoice.value = "rock";}
-  else if (choiceNum == 1){compChoice.value = "paper";}
-  else if (choiceNum == 2){compChoice.value = "scissors";}
-  
+  if (choiceNum == 0){compChoice.Value = "rock";}
+  else if (choiceNum == 1){compChoice.Value = "paper";}
+  else if (choiceNum == 2){compChoice.Value = "scissors";}
   return choiceNum;
 }
 
 function playRound (){
-  let winArray = [[0, 2, 1], [1, 0, 2], [2, 1, 0]];
-  let result = winArray[playerChoiceInt][compChoiceInt];
+  let win_Array = [[0, 2, 1], [1, 0, 2], [2, 1, 0]];
+  let result = win_Array[playerChoiceInt][compChoiceInt];
 
   if (result == 0){
     output.textContent = `Its a tie! You chose ${playerChoice} and The computer chose ${compChoice.Value}`;
@@ -49,23 +47,29 @@ function playRound (){
     output.textContent = `You won! You chose ${playerChoice} and The computer chose ${compChoice.Value}`;
     playerScore++
   }
-  else if (result == 1){
+  else if (result == 2){
     output.textContent = `You lost! You chose ${playerChoice} and The computer chose ${compChoice.Value}`;
-    computerScore++
+    compScore++
   }
 }
 
 function playGame(){
-  output.textContent = "Choose Rock, Paper, or Scissors"
-
+  output.textContent = "Choose Rock, Paper, or Scissors";
   playRound();
-
   player.textContent = `Player Score: ${playerScore}`;
-  computer.textContent = `Computer Score: ${computerScore}`;
-
+  computer.textContent = `Computer Score: ${compScore}`;
+  
   if (playerScore == 5){
     output.textContent = "You won the game!"
-  }else if (computerScore == 5){
+    playerScore = 0;
+    compScore = 0;
+    player.textContent = `Player Score: ${playerScore}`;
+    computer.textContent = `Computer Score: ${compScore}`;
+  }else if (compScore == 5){
     output.textContent = "You lost the game!"
+    playerScore = 0;
+    compScore = 0;
+    player.textContent = `Player Score: ${playerScore}`;
+    computer.textContent = `Computer Score: ${compScore}`;
   }
 }
